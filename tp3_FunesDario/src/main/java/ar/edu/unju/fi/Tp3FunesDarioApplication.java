8e9075e672c21a3221e4282e792108e5575739de
dario <dario@DESKTOP-F43KC1A>
2024-05-09 17:10:52 -0300

    Se creo el proyecto vacío para el tp3 del tipo spring starter proyect y se agrego el index.html con pequeños cambios

---
+++ tp3_FunesDario/src/main/java/ar/edu/unju/fi/Tp3FunesDarioApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3FunesDarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3FunesDarioApplication.class, args);
+	}
+
+}
